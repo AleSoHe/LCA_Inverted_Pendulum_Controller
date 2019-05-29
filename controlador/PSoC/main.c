1864e3e913e49eb3ee192bc790994cb4a0650b95
Max Hronek <30559609+maxhr08@users.noreply.github.com>
2019-05-28 18:34:15 -0600

    Update main.c

--- controlador/PSoC/main.c
+++ controlador/PSoC/main.c
@@ -29,32 +29,34 @@
 
 /* Global constants */
 #define PWM_MAX         499   // PWM 100% (499) with Tpwm = 500us @ 1MHz (500 counts)
-#define PWM_FACTOR      15   // 100 counts/volt with Tpwm = 500us @ 1MHz
+#define PWM_FACTOR      300   // 100 counts/volt with Tpwm = 500us @ 1MHz
 #define VDAC_FACTOR     62.5  // 1bit/0.016mV
 #define VDAC_MAX        300   // 4.8V max. with DVDAC 9 bits and PGA x2
 #define POT_CONST       3100  // Max. counts to 100%
 
 /* PI algorithm constants */
-#define REFERENCE      0    // Angle reference
+#define REFERENCE      -0.25 //-0.15    // Angle reference
+#define PosREFERENCE    0   //-0.15    // Position reference
 #define MAXINTEGRAL    4.7  // Limits the integral part to 4.7V
 #define TS_FACTOR      1    // 5ms counts for Ts = 5ms
-
+#define N              0.3    // Filter
 /* isr global variables */
 
 volatile int8 factor = TS_FACTOR;    // Counter for Ts = 5ms when Timer runs faster than 5ms
 
 /* Buttons variables */
 bool idle = true;  // Signals when step input can be applied
-bool run = false;  // Clock to advance Display state machine
 bool save = false; // flag to preserve PI constants adjustment
 
 /* Button sync enumerated type */
 enum  DebounceState {WAIT, RUN, PRESS};
 
 /* PID algorithm global variables */
-#define KP -0.21       // Proportional default constant
-#define KI -0.158      // Integral default constant @ 5ms
-#define KD -0.0056     // Derivative constant
+#define KPpos -0.05       // Proportional default constant
+
+#define KP -0.21    //-0.6        // Proportional default constant
+#define KI -0.158   //-0.158      // Integral default constant @ 5ms
+#define KD -0.0056    // Derivative constant
 #define Ts 0.005
 float KIT = Ts*KI;
 float KDT = KD/Ts;
@@ -64,10 +66,10 @@ volatile float dk = 0; // Derivative action and memory
 int PositionSend;
 float AngleSend;
 
-void Send(float Ang) //uint16 Pwm, uint16 Pos,
+void Send(float Ang, int Pos) //uint16 Pwm, uint16 Pos,
 {                   
     char Msg[50] = {};
-    sprintf(Msg,"%f \r\n",Ang);// %d;%d Pwm,Pos,
+    sprintf(Msg,"%f;%d \r\n",Ang,Pos);// %d;%d Pwm,Pos,
     
     USBUART_PutString(Msg);        
 }
@@ -77,16 +79,18 @@ CY_ISR_PROTO(isr_Timer_Handler);
 
 // Interrupt handler declaration
 CY_ISR(isr_Timer_Handler){
-    
+    //Ex_time_Write(1);
     /* ISR PI algoritm local variables */
     float yk = 0; 	 // speed feedback
     float med = 0;  // medición de sensor de ángulo
     float ek = 0;   // speed error
-    float lek= 0;   //last error*/ //For D control
+    float epk = 0;   // position error
+    float lek = 0;   //last error*/ //For D control
     float mk = 0;   // total control action
-    //int16 vdac = 0; // For VDAC output
-    int16 data = 0; // For ADC reading
-
+    float lmk = 0;  //
+    int16 data = 0; // For ADC reading    
+    int Position = 0;
+    
     #if (PWM_Resolution)    
         int16 pwm = 0;  // For PWM output
     #endif
@@ -97,15 +101,25 @@ CY_ISR(isr_Timer_Handler){
 
     if ( (--factor <= 0) && (idle == true)) {
         factor = TS_FACTOR;
-
+        
         // Reads angle value
         data = ADC_Read16();
-
+        
+        // Reads position value
+        Position = QuadDec_GetCounter();
+        if(Position > 32767){
+            Position = Position -65535;   
+        }
+        PositionSend = Position;
+        
+        //Position error 
+        epk = PosREFERENCE - Position;
+        
         // Medición de sensor de ángulo
         med = ADC_CountsTo_Volts((int32) data);
-
+        
         // Cálculo de ángulo en grados
-        yk = med*77.4056326 + 163; //163.553089
+        yk = med*77.4056326 + 157; //161.6 //157.553089
         if(yk>180){
             yk = yk - 360;
         }
@@ -115,25 +129,28 @@ CY_ISR(isr_Timer_Handler){
         
         lek = ek;
         ek = REFERENCE - yk; // Follow the reference
-
+        
         /* PI control algorithm calculation */        
 
         /* Integral part, the rightmost term is also ik_1 */
         ik = KIT*lek + ik;
 
         /*Derivate calcutation*/
-        dk=KDT*(ek-lek);
+        
+        dk=KDT*N*(ek-lek)-lmk*(N*Ts-1);
+        
+        //dk=KDT*(ek-lek);
 
         /* Total PI control action */ 
         //mk = KP*ek + ik;
 
         /* Total PID control action */
-        mk = KP*ek + ik + dk;
+        mk = KPpos*epk; //mk = KP*ek + ik + dk + KPpos*epk; //
         
         /* PWM conditional use */
         #if defined CY_PWM_PWM_H
             /* Scales mk to PWM range */
-            pwm = (int16) (fabs(mk)*PWM_FACTOR);
+            pwm = 220 + (int16) (fabs(mk)*PWM_FACTOR); //255+
 
             /* Establish motor direction*/
             if(mk > 0){
@@ -151,10 +168,6 @@ CY_ISR(isr_Timer_Handler){
             if (pwm > PWM_MAX){ 
                 pwm = PWM_MAX;
             }
-            else if (pwm < 0) {
-                pwm = 0; 
-            }
-
             PWM_WriteCompare(pwm); // Outputs PWM saturated value
         #endif     
                     
@@ -163,15 +176,14 @@ CY_ISR(isr_Timer_Handler){
             ik = MAXINTEGRAL;
         else
         if (ik < -MAXINTEGRAL) ik = -MAXINTEGRAL;
-        
+    //Ex_time_Write(0);    
     }//if
     
 } // CY_ISR
 
 CY_ISR(InterruptTimer){
-    Timer_UART_WritePeriod(500000);
-    
-    Send(AngleSend);
+    Timer_UART_WritePeriod(5000);
+    Send(AngleSend,PositionSend);//Send(AngleSend);
 }
 
 int main(void){
@@ -181,22 +193,19 @@ int main(void){
     
     //uint16 Encoder_count;
     
-    uint16 Position = 0;
-    
-    
     /* Startup code for all blocks */
     ADC_Start();
     Timer_Start();
     Timer_UART_Start();
     PWM_Start();
-    
+    QuadDec_Start();
     
     /* Start USBFS operation with 5-V operation. */
     USBUART_Start(USBFS_DEVICE, USBUART_5V_OPERATION);
     
     /* Correction for ADC_CountsTo_Volts */
-    ADC_SetOffset(-1); // -1mV offset correction
-    ADC_SetGain(673);  // 0.9895 gain correction
+    //ADC_SetOffset(-1); // -1mV offset correction
+    //ADC_SetGain(673);  // 0.9895 gain correction
 
     /* PWM conditional use */
     #if defined CY_PWM_PWM_H    
@@ -240,7 +249,6 @@ int main(void){
                     }
                     
                     /* Send data back to host. */
-                    PositionSend = Position;
                     
                     if (USBUART_BUFFER_SIZE == count)
                     {
